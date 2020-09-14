793df87b6cfa358d302e173c118fd75e1d7a5983
LukasKaselionis <l.kaselionis@gmail.com>
2020-09-14 14:46:03 +0300

    create and render header element

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return <header className="header">
+        <h2 className="header__title">react image gallery</h2>
+        <h4 className="header__description">The image gallery is divided into two columns using Bootstrap library</h4>
+    </header>
+}
+
+export default Header;
