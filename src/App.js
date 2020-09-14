ba985f93d6f27a5ccaaa3d49228ae6bef5bc3646
LukasKaselionis <l.kaselionis@gmail.com>
2020-09-14 20:47:04 +0300

    fetched unsplash api data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 import Header from './components/Header/Header';
 import Loader from './components/Loader/Loader';
 
 class App extends Component {
   state = {
-    loading: false
+    loading: false,
+    images: []
+  }
+
+  componentDidMount() {
+    this.fetchImages();
+  }
+
+  fetchImages = () => {
+    axios.get('https://api.unsplash.com/photos/random', {
+      params: {
+        count: 20,
+      },
+      headers: {
+        Authorization: 'Client-ID y2pheNRSCVBvPOgdZ29TwMu7p3gn0DLgcqCzdEXvWGM'
+      }
+    })
+      .then(response => {
+        this.setState({ images: [...this.state.images, ...response.data] });
+        console.log(response.data);
+      })
+      .catch(err => {
+        if (err.response) {
+          console.log(err.response);
+        }
+      });
   }
 
   render() {
