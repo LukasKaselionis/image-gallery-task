e9764f0f5b96c70b408f97b09fb6633bef316cad
LukasKaselionis <l.kaselionis@gmail.com>
2020-09-14 15:05:30 +0300

    adding loader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Loader from './components/Loader/Loader';
 
 class App extends Component {
+  state = {
+    loading: false
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
+        <Loader />
       </div>
     );
   }
